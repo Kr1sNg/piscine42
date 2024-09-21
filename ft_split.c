0f882c07e76309c97341a3c1fe0064ef454134eb
Kr1s Ng <piq.k0olz@gmail.com>
2024-09-22 00:01:43 +0200

    final

--- ft_split.c
+++ ft_split.c
@@ -46,15 +46,14 @@ int	count_words(char *str)
 	count = 0;
 	while (str[i] != '\0')
 	{
-		while (space_check(str[i]) == 1)
+		while (str[i] != '\0' && space_check(str[i]) == 1)
 			i++;
-		if (space_check(str[i]) == 0)
+		if (str[i] != '\0' && space_check(str[i]) == 0)
 		{
 			count++;
-			while(space_check(str[i]) == 0)
+			while(str[i] != '\0' && space_check(str[i]) == 0)
 				i++;
 		}
-		i++;
 	}
 	return (count);
 }
@@ -66,25 +65,28 @@ int	count_letter(char *str)
 
 	count = 0;
 	i = 0;
-	while (str[i] != 0)
-	{
-		if (space_check(str[i]) == 0)
-			count++;
+	while (str[i] != 0 && space_check(str[i]) == 0)
+    {
+        count++;
 		i++;
 	}
 	return (count);
 }
 
-char	*ft_strcat(char *str)
+char	*ft_strcat(char *src)
 {
 	char	*arr;
-	int	i;
-	
-	arr = malloc(sizeof(char) * (count_letter(str) + 1);
+	int	    i;
+    int     len;
+    
+    len = count_letter(src);
+	arr = malloc(sizeof(char) * (len + 1));
+    if (arr == NULL)
+        return (NULL);
 	i = 0;
-	while ((str[i] != '\0') && space_check(str[i]) == 0))
+	while (i < len)
 	{
-		arr[i] = str[i];
+		arr[i] = src[i];
 		i++;
 	}
 	arr[i] = '\0';
@@ -94,56 +96,48 @@ char	*ft_strcat(char *str)
 
 char	**ft_split(char *str)
 {
-	int	i;
+	int    i;
+    	int    j;
 	char	**arrs;
-	*arrs = malloc(sizeof(char *) * (count_words(str) + 1));
-	if (*arrs == NULL)
+	arrs = malloc(sizeof(char *) * (count_words(str) + 1));
+	if (arrs == NULL)
 		return (NULL);
 	i = 0;
 	j = 0;
 	while (str[i] != '\0')
 	{
-		while (space_check(str[i]) == 1)
+		while (str[i] != '\0' && space_check(str[i]) == 1)
 			i++;
-		if (space_check(str[i]) == 0)
+		if (space_check(str[i]) == 0 && str[i] != '\0')
 		{
-			arrs[j] = ft_strcat(str);
+			arrs[j] = ft_strcat(str + i);
 			j++;
-			while (space_check(str[i]) == 0)
+			while (space_check(str[i]) == 0 && str[i] != '\0')
 				i++;
 		}
-		i++;
 	}
-	arrs[j] = '\0';
+	arrs[j] = NULL;
 	return (arrs);
 }
 
 #include <stdio.h>
 
-int	main(int ac, char **av)
-{
-	char **arrs;
-	char *str = " Hello, The New\t  WOR\nLD  ! ";
-	
-	arrs = ft_split(str);
-	printf("%s\n", arrs[0]);
-
-	free(arrs);
-}
-			
-	 
-
-
-
-
-
-#include <stdio.h>
-
 int	main(void)
 {
-	char *str = "There are four words . ";
-	printf("%d\n", count_words(str));
-	printf("%i", count_letter(str));
+    char **arrs;
+    char *str = " Hello, The New\t  WOR\nLD  ! ";
+    int     i = 0;
+    arrs = ft_split(str);
+    if (arrs == NULL)
+        return (1);
+    while (arrs[i] != NULL)
+    {
+        printf("%s\n", arrs[i]);
+        free(arrs[i]);
+        i++;
+    }
+    free(arrs);
+    return (0);
 }
 
 
