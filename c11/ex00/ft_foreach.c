5d4b06fff9443be708e08dd9ed6307a5e367b657
Kr1sNg <piq.k0olz@gmail.com>
2024-10-03 16:27:45 +0200

    piscine42

--- c11/ex00/ft_foreach.c
+++ c11/ex00/ft_foreach.c
@@ -6,8 +6,46 @@
 /*   By: tat-nguy <tat-nguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 12:12:44 by tat-nguy          #+#    #+#             */
-/*   Updated: 2024/10/02 12:18:21 by tat-nguy         ###   ########.fr       */
+/*   Updated: 2024/10/03 15:36:06 by tat-nguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_foreach(int *tab, int length, void(*f)(int))
+void	ft_foreach(int *tab, int length, void (*f)(int))
+{
+	int		i;
+
+	i = 0;
+	while (i < length)
+	{
+		f(tab[i]);
+		i++;
+	}
+}
+
+/*#include <unistd.h>
+void	ft_putnbr(int nbr)
+{
+	char	c;
+	long	nb = nbr;
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		c = nb + '0';
+		write(1, &c, 1);
+	}
+}
+
+int main(void)
+{
+	int		tab[5] = {1, 5, 12, 999, 2345678};
+	ft_foreach(tab, 5, &ft_putnbr);
+}*/
